a3a6bf979583313a67e4217bad2e3b481a973705
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-05-01 09:16:49 -0300

    validando o body

--- src/test/java/br/learning/restassured/LearningTest.java
+++ src/test/java/br/learning/restassured/LearningTest.java
@@ -1,18 +1,33 @@
 package br.learning.restassured;
 
+import org.apache.http.HttpStatus;
 import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class LearningTest {
 
     @Test
     public void testMatcherAssert() {
-        MatcherAssert.assertThat("Hello", Matchers.isA(String.class));
-        MatcherAssert.assertThat(123, Matchers.isA(Integer.class));
+        MatcherAssert.assertThat("Hello", isA(String.class));
+        MatcherAssert.assertThat(123, isA(Integer.class));
         Assertions.assertInstanceOf(String.class, "Hello");
     }
 
+    @Test
+    public void testValidandoBody() {
+
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(notNullValue())
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"));
+    }
 
 }
