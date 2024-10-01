4553ab8d9dd9e40485dad1811128d1d967bca517
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-10-01 00:49:53 -0300

    wip commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(){
+            {
+                add(1);
+                add(2);
+                add(3);
+            }
+        };
+
+        list.forEach(item -> );
+    }
+}
